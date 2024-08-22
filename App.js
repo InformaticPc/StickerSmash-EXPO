e590b580586cd02dc5821dfde88ecb8346bd6180
Informatic PC <informaticpc07@gmail.com>
2024-08-22 13:32:11 +0200

    Adding first buttons

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
-import ImageViewer from './components/ImageViewer';
+import Button from "./components/Button.js";
+import ImageViewer from './components/ImageViewer.js';
 
 const PlaceHolderImage = require('./assets/images/background-image.png')
 
@@ -11,21 +12,29 @@ export default function App() {
       <View styles={styles.imageContainer}>
          <ImageViewer placeHolderImageSource={PlaceHolderImage}/>
       </View>
+
+      <View style={styles.footerContainer}>
+        <Button label ="Choose a photo"/>
+        <Button label= "Use this photo"/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  footerContainer:{
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
   container: {
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
   imageContainer:{
     flex: 1,
-    padding: 58,
+    paddingTop: 58,
   },
-  
+  //SOMETHNG WRONG DON'T DISPLAY NOTHING
 });
