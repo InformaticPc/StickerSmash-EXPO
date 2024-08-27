7531eee23dedb5135d6eda83dc0aca14e9d6fa17
Informatic PC <informaticpc07@gmail.com>
2024-08-27 12:05:51 +0100

    Initial commit in win at home before checking the diff of code

---
+++ EXPO/App.js
@@ -0,0 +1,40 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View} from "react-native";
+
+import Button from './components/Button'; 
+import ImageViewer from './components/ImageViewer';
+
+const PlaceholderImage = require("./assets/images/background-image.png");
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" theme={"primary"}/>
+        <Button label="Use this photo" />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    //flex: 1,
+    paddingTop: 3,
+    marginBottom:10,
+  },
+  footerContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent:'center',
+  },
+});
